a640092a99f6eb5a4036de940716d2c2371dc19e
Панов Илья <panov210@yandex.ru>
2020-01-22 21:49:13 +0300

    Update CString.php

--- CString.php
+++ CString.php
@@ -1,10 +1,16 @@
 <?php
 
-class CString {
+class CString implements Iterator, ArrayAccess {
 	private $_value, $_length;
 	
 	public function __construct($value) {
-		$this->_value = $value;
+		if (is_string($value))
+			$this->_value = $value;
+		elseif (is_int($value))
+			$this->_value = chr($value);
+		else
+			throw new InvalidArgumentException('value has invalid type');
+		
 		$this->_length = strlen($this->_value);
 	}
 	
@@ -16,20 +22,65 @@ class CString {
 		return $this->_value;
 	}
 	
+	//---------------------------------------------------
+	
+	private $_iteratorPosition = 0, $_iteratorCurrent = false;
+	
+	public function rewind() {
+        $this->_iteratorPosition = 0;
+		$this->_iteratorCurrent = false;
+    }
+
+    public function current() {
+        return $this->_iteratorCurrent;
+    }
+
+    public function key() {
+        return $this->position;
+    }
+
+    public function next() {
+        $this->_iteratorPosition++;
+		$this->_iteratorCurrent = substr($this->_value, $this->_iteratorPosition, 1);
+    }
+
+    public function valid() {
+		return $this->_iteratorPosition >= 0 && $this->_iteratorPosition < strlen($this->_value);
+    }
+	
+	//---------------------------------------------------
+	
+	public function offsetSet($offset, $value) {
+		if (is_null($offset))
+			$this->_value .= $value;
+		else
+			$this->_value = substr_replace($this->_value, $value, $offset, 0);
+    }
+
+    public function offsetExists($offset) {
+        return $offset >= 0 && $offset < strlen($this->_value);
+    }
+
+    public function offsetUnset($offset) {
+		$this->_value = substr_replace($this->_value, '', $offset, 1);
+    }
+
+    public function offsetGet($offset) {
+		return $this->offsetExists($offset) ? substr($this->_value, $offset, 1) : null;
+    }
+	
+	//---------------------------------------------------
+	
 	public function length() {
 		return $this->_length;
 	}
 	
-	public function chunks($length = 1) {
-		return array_map(['static', 'of'], str_split($this->_value, $length));
+	public function isEmpty() {
+		return $this->_value === '';
 	}
 	
-	public function indexOf($search, $offset = 0, $ignoreCase = false) {
-		return $ignoreCase ? stripos($this->_value, $search, $offset) : strpos($this->_value, $search, $offset);
-	}
-	
-	public function lastIndexOf($search, $offset = 0, $ignoreCase = false) {
-		return $ignoreCase ? strripos($this->_value, $search, $offset) : strrpos($this->_value, $search, $offset);
+	public function isWhiteSpace() {
+		return trim($this->_value) === '';
 	}
 	
 	public function startsWith($search) {
@@ -40,13 +91,37 @@ class CString {
 		return substr($this->_value, $this->_length - strlen($search)) == $search;
 	}
 	
-	public function lower() {
+	public function indexOf($search, $offset = 0, $ignoreCase = false) {
+		$searcher = $ignoreCase ? 'stripos' : 'strpos';
+		return $searcher($this->_value, $search, $offset);
+	}
+	
+	public function lastIndexOf($search, $offset = 0, $ignoreCase = false) {
+		$searcher = $ignoreCase ? 'strripos' : 'strrpos';
+		return $searcher($this->_value, $search, $offset);
+	}
+	
+	public function code($offset = 0) {
+		return ord(substr($this->_value, $offset, 1));
+	}
+	
+	public function chunks($length = 1) {
+		return array_map(['static', 'of'], str_split($this->_value, $length));
+	}
+	
+	//---------------------------------------------------
+	
+	public function toLower() {
 		return new self(strtolower($this->_value));
 	}
 	
-	public function upper() {
+	public function toUpper() {
 		return new self(strtoupper($this->_value));
-	}	
+	}
+	
+	public function capitalize() {
+		return new self(lcfirst($this->_value));
+	}
 	
 	public function join($items) {
 		return new self(implode($this->_value, $items));
@@ -67,9 +142,13 @@ class CString {
 		return new self($result);
 	}
 	
-	public function slice($start, $length = false) {
-		$result = $length !== false ? substr($this->_value, $start, $length) : substr($this->_value, $start);
+	public function slice($offset, $length = false) {
+		$result = $length !== false ? substr($this->_value, $offset, $length) : substr($this->_value, $offset);
 		return new self($result);
+	}
+	
+	public function insert($str, $offset) {
+		return new self(substr_replace($this->_value, $str, $offset, 0));
 	}
 	
 	public function trim($chars = " \t\n\r\0\x0B") {
@@ -84,8 +163,17 @@ class CString {
 		return new self(rtrim($this->_value, $chars));
 	}
 	
-	public function replace($search, $replace, &$count = false) {
-		return new self(str_replace($search, $replace, $this->_value, $count));
+	public function replace($search, $replace, &$count = false, $ignoreCase = false) {
+		$replacer = $ignoreCase ? 'str_ireplace' : 'str_replace';
+		return new self($replacer($search, $replace, $this->_value, $count));
+	}
+	
+	public function duplicate($count) {
+		return new self(str_repeat($this->_value, $count));
+	}
+	
+	public function shuffle() {
+		return new self(str_shuffle($this->_value));
 	}
 	
 	public function format(...$params) {
